4fa45f7cd2ad1841d62e817b4c3edbe97420342a
marco <marcovanblijderveen@gmail.com>
2025-04-15 16:12:34 +0200

    Les 10

--- helper.py
+++ helper.py
@@ -1,7 +1,21 @@
-def decoreer(tekst=""):
-    lengte = len(tekst) + 4
+def decoreer(tekst=""):    
+    tekst="header"    
+    lengte = len(tekst) + 4    
+    print()    
+    print(lengte * "*")    
+    print(f"* {tekst} *")    
+    print(lengte * "*")    
     print()
-    print(lengte * "*")
-    print(f"* {tekst} *")
-    print(lengte * "*")
-    print()
+
+def fooi_pp(bedrag, personen):    
+    bedrag_pp = bedrag/personen    
+    return f"Het bedrag per persoon is {bedrag_pp} euro"
+
+def onderstreep(tekst=""):    
+    uit = []    
+    uit.append(tekst)    
+    uit.append(len(tekst) * "=")    
+    return uit
+
+def som(dictionary):
+    return sum(dictionary.values())
