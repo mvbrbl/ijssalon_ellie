7563c064b27a553e09dd51322f601ccdaa4098de
marco <marcovanblijderveen@gmail.com>
2025-04-15 16:18:48 +0200

    les 10 functies toegevoegd

--- boekhouding.py
+++ boekhouding.py
@@ -16,7 +16,8 @@ totaal_inkomsten = som(inkomsten)
 # Toon de presentatie van de inkomsten
 presenteer(inkomsten, totaal_inkomsten)
 
-with open('boekhouding.csv’, 'w',newline='') as csvfile:
+with open('boekhouding.csv', 'w', newline='') as csvfile:
     for key, value in inkomsten.items():
-    writer = csv.writer(csvfile, delimiter=';')
-    writer.writerow([key,value])
+     writer = csv.writer(csvfile, delimiter=';')
+     writer.writerow([key,value])
+
